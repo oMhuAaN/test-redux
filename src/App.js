8503c54d91574dc05e7d07f857bf085745e18959
haibin@SuperAMD <894457272@qq.com>
2021-02-05 23:48:51 +0800

    423432432

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Input, Button, List } from 'antd';
+import store from './redux/store';
+import {
+  CHANGE_INPUT_VALUE,
+  ADD_TODO_ITEM,
+  DELETE_TODO_ITEM
+} from './redux/actionTypes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    store.subscribe(this.handleStoreChange);
+  }
+  state = store.getState();
+  handleInputChange = (e) => {
+    const action = {
+      type: CHANGE_INPUT_VALUE,
+      value: e.target.value,
+    }
+    store.dispatch(action);
+  }
+  handleStoreChange = () => {
+    this.setState(store.getState());
+  }
+  handleBtnClick = () => {
+    const action = {
+      type: ADD_TODO_ITEM,
+    }
+    store.dispatch(action);
+  }
+  handleItemDelete = (index) => {
+    const action = {
+      type: DELETE_TODO_ITEM,
+      index
+    }
+    store.dispatch(action);
+  }
+  render() {
+    return (
+      <div style={{
+        margin: 10,
+      }}>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+        }}>
+          <Input
+            placeholder="todo info"
+            style={{
+              marginRight: 10,
+              width: 300,
+            }}
+            value={this.state.inputValue}
+            onChange={this.handleInputChange}
+          />
+          <Button
+            type="primary"
+            onClick={this.handleBtnClick}
+          >提交</Button>
+        </div>
+        <List
+          style={{
+            width: 300,
+            marginTop: 10,
+          }}
+          bordered
+          dataSource={this.state.list}
+          renderItem={(item, index) => (
+            <List.Item
+              onClick={() => {
+                this.handleItemDelete(index);
+              }}
+            >
+              {item}
+            </List.Item>
+          )}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
